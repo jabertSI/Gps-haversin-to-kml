34d39013d3967cd64f288b210352b4284c33cb34
Jabert SI CHAIB <jabert.sichaib@lecole-ldlc.com>
2016-10-27 16:34:29 +0200

    Update partie_1.py

--- partie_1.py
+++ partie_1.py
@@ -1,4 +1,4 @@
-from math import *
+from math import radians, cos, sin, atan2, sqrt, ceil
 
 terre = 6371
 # LECTURE DU FICHIER
@@ -12,16 +12,19 @@ for adr in carnet:
     adr[1] = float(adr[1])
     adr[2] = float(adr[2])
     print(adr[0] + ' : latitude = ' + str(adr[1]) + ' : longitude = ' + str(adr[2]) + ': altitude = ' + adr[3] + "m")
+
+for ville1 in carnet:
+    ville1[1], ville1[2] = map(radians, [ville1[1], ville1[2]])
+
 # Generateur de couple
 for ville1 in carnet:
     for ville2 in carnet:
         if ville1 != ville2:
             # Calcule de la distance.
             # conversion degre en radian
-            ville1[1], ville1[2], ville2[1], ville2[2] = map(radians, [ville1[1], ville1[2], ville2[1], ville2[2]])
             lat = ville2[1] - ville1[1]
             lng = ville2[2] - ville1[2]
             d = sin(lat * 0.5) ** 2 + cos(ville1[1]) * cos(ville2[1]) * sin(lng * 0.5) ** 2
-            h = 2 * asin(sqrt(d))
+            h = 2 * atan2(sqrt(d),sqrt(1-d))
             distance = h * terre
-            print(ville1[0], ville2[0], ":", ceil(distance), "km")  # 90 couple possible
+            print("La distance entre", ville1[0],"et", ville2[0], "est de", ceil(distance), "km")  # 90 couple possible
